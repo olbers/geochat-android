0ea1542d6cb134b052f065edf4cc5134fd252caa
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-07-27 15:04:58 +0700

    Login is not an activity which you can go back to

--- org.instedd.geochat.android/src/org/instedd/geochat/LoginActivity.java
+++ org.instedd.geochat.android/src/org/instedd/geochat/LoginActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,16 +23,30 @@ public class LoginActivity extends Activity {
 	    
 	    final GeoChatSettings settings = new GeoChatSettings(this);
 	    
+	    String existingUser = settings.getUser();
+	    String existingPassword = settings.getPassword();
+	    
 	    final EditText uiUser = (EditText) findViewById(R.id.user);
 	    final EditText uiPassword = (EditText) findViewById(R.id.password);
 	    final TextView uiFeedback = (TextView) findViewById(R.id.feedback);
+	    final Button uiLogin = (Button) findViewById(R.id.login_button); 
 	    
-	    uiUser.setText(settings.getUser());
-	    uiPassword.setText(settings.getPassword());
+	    uiUser.setText(existingUser);
+	    uiPassword.setText(existingPassword);
 	    
-	    ((Button) findViewById(R.id.login_button)).setOnClickListener(new OnClickListener() {
+	    // Automatic login (no server authentication so that the user can view offline content)
+	    if (!TextUtils.isEmpty(existingUser) && !TextUtils.isEmpty(existingPassword)) {
+	    	uiLogin.setEnabled(false);
+	    	uiFeedback.setTextColor(Color.GREEN);
+			uiFeedback.setText(R.string.logging_in_to_geochat);
+			enterGeoChat();
+	    	return;
+	    }
+	    
+	    uiLogin.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				uiLogin.setEnabled(false);
 				uiFeedback.setTextColor(Color.GREEN);
 				uiFeedback.setText(R.string.logging_in_to_geochat);
 				
@@ -42,17 +57,24 @@ public class LoginActivity extends Activity {
 				try {
 					if (api.credentialsAreValid()) {
 						settings.setUserAndPassword(user, password);
-						startActivity(new Intent().setClass(LoginActivity.this, GeoChatTabsActivity.class));
+						enterGeoChat();
 					} else {
+						uiLogin.setEnabled(true);
 						uiFeedback.setTextColor(Color.RED);
 						uiFeedback.setText(R.string.invalid_credentials);
 					}
 				} catch (Exception e) {
+					uiLogin.setEnabled(true);
 					uiFeedback.setTextColor(Color.RED);
 					uiFeedback.setText(R.string.unknown_error_maybe_no_connection);
 				}
 			}
 		});
+	}
+	
+	private void enterGeoChat() {
+		startActivity(new Intent()
+			.setClass(LoginActivity.this, GeoChatTabsActivity.class));
 	}
 
 }
