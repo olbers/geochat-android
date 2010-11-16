8928799dcae3b44225a2ed2e59bcf56e4e633272
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-11-16 11:33:00 -0300

    Added refresh rate option

--- org.instedd.geochat.android/src/org/instedd/geochat/GeoChatPreferences.java
+++ org.instedd.geochat.android/src/org/instedd/geochat/GeoChatPreferences.java
@@ -3,11 +3,14 @@ package org.instedd.geochat;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.text.TextUtils;
 
 public class GeoChatPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+	
+	private GeoChatSettings settings;
 	
 	@Override
     protected void onCreate(Bundle icicle) {
@@ -16,12 +19,16 @@ public class GeoChatPreferences extends PreferenceActivity implements OnSharedPr
         getPreferenceManager().setSharedPreferencesName(GeoChatSettings.SHARED_PREFS_NAME);
         getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         
+        this.settings = new GeoChatSettings(this);
         updateGeoChatNumber();
+        updateRefreshRate();
     }
 
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 		if (GeoChatSettings.GEOCHAT_NUMBER.equals(key)) {
 			updateGeoChatNumber();
+		} else if (GeoChatSettings.REFRESH_RATE.equals(key)) {
+			updateRefreshRate();
 		}
 	}
 
@@ -32,6 +39,18 @@ public class GeoChatPreferences extends PreferenceActivity implements OnSharedPr
 			preference.setSummary(R.string.geochat_number_summary);
 		} else {
 			preference.setSummary(getResources().getString(R.string.geochat_number_set_summary, number));
+		}
+	}
+	
+	private void updateRefreshRate() {
+		String refreshRate = String.valueOf(settings.getRefreshRateInMinutes());
+		ListPreference preference = (ListPreference) findPreference(GeoChatSettings.REFRESH_RATE);
+		for (int i = 0; i < preference.getEntryValues().length; i++) {
+			CharSequence entry = preference.getEntryValues()[i];
+			if (entry.equals(refreshRate)) {
+				preference.setSummary(preference.getEntries()[i]);
+				break;
+			}
 		}
 	}
 
